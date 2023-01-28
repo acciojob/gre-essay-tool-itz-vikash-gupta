30f47b7e102812edb911784aeedb82139a63b40f
itz-vikash-gupta <120251402+itz-vikash-gupta@users.noreply.github.com>
2023-01-28 16:24:14 +0530

    do

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+function count_word(val) {
+    var wom = val.split(" ");
+    if (wom[0]=="")return {words:0}
+    return {
+      words: wom ? wom.length : 0,
+    };
+  }
+  var textContent = document.getElementById("evaluatedText");
+  var showWordCount = document.getElementById("wordCount");
+  
+  textContent.addEventListener(
+    "input",
+    function () {
+      var v = count_word(this.value);
+      showWordCount.innerHTML = v.words;
+    },
+    false
+  );
